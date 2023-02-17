f73dcea37e790421ff52e5cf4b56688dc6f4fb47
aristides1000 <aristides1000@gmail.com>
2023-02-17 06:26:44 -0400

    hist finalizado

---
+++ historias_graciosas.py
@@ -0,0 +1,9 @@
+adjetivo = input("Adjetivo: ")
+verbo1 = input("Verbo: ")
+verbo2 = input("Verbo: ")
+persona_famosa = input("Persona Famosa: ")
+
+historia_graciosa = f"La programación informática es tan {adjetivo}! Me emociona mucho todo el tiempo porque \
+me encanta {verbo1}. Mantente hidratado y {verbo2} como {persona_famosa}!"
+
+print(historia_graciosa)
